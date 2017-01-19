3ab3513d830748e480b9623b9fded480f0e1f062
Lee Irving Barlin <lbarlin@outlook.com>
2017-01-19 23:32:42 +0800

    init

---
+++ RLPh.WeChatAuth.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RLPh.WeChatAuth.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Newtonsoft.Json.Linq.JObject.Parse(json).ToString(Formatting.Indented)
+            
+            return View();
+        }
+
+        public ActionResult WeChatAuthResponse()
+        {
+            return View();
+        }
+    }
+}
---
+++ RLPh.WeChatAuth.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RLPh.WeChatAuth.Web.MvcApplication" Language="C#" %>
